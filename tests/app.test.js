3fa4dd22b6b7eb2cb4754a23efea6ab3c7a17e6c
FernandaRosas2828 <2023171019@uteq.edu.mx>
2026-02-25 17:50:28 -0600

    Provocando error en test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../appp');
 const { calculateValue } = require('../lib/logic');
 
 describe('Suite de Pruebas de Calidad de Software', () => {
